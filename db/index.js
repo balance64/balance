d39f7cd53f81857758fef82221ebc5c094be0c16
Fliko <baxter.flw@gmail.com>
2017-05-01 18:36:32 -0500

    Add cleardb addon to heroku'

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'balance'
+  host: 'ef1f2289@us-cdbr-iron-east-03.cleardb.net',
+  user: 'b8a31a653b0b39',
+  password: 'ef1f2289',
+  database: 'heroku_55758f1ee80e743'
 });
 
 connection.connect(function(err) {
