6b626116a658b5b29afc6edb55272dfa59e0f1ce
Fliko <baxter.flw@gmail.com>
2017-05-01 17:56:35 -0500

    Stop hardcoding ports like a noob

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ var db = require('../db/index.js');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('../client'));
 
 app.get('/theTruth', (req, res) => {
@@ -13,6 +15,6 @@ app.get('/theTruth', (req, res) => {
 	});
 });
 
-app.listen(4568, function() {
-  console.log('listening on 4568...');
-})
+app.listen(app.get('port'), function() {
+  console.log('Balance running on port ... ', app.get('port'));
+});
