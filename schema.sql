094843f51ecc3180dab7f2c1450160a4363afbe3
Fliko <baxter.flw@gmail.com>
2017-05-02 03:47:00 -0500

    heroku db now has users table

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS balance;
-CREATE DATABASE balance;
+-- DROP DATABASE IF EXISTS balance;
+-- CREATE DATABASE balance;
 
-USE balance;
+USE heroku_ec7185f66b0bf5c;
 
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
