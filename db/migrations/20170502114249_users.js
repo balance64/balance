b6ccb1dd1e1b188ebaa32f4da831a0fd30b5758f
Fliko <baxter.flw@gmail.com>
2017-05-02 14:54:45 -0500

    working knex db and server

---
+++ db/migrations/20170502114249_users.js
@@ -0,0 +1,49 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTableIfNotExists('users', (col) => {
+    col.increments('id').primary();
+    col.string('username').unique().notNullable();
+    col.string('password').notNullable();
+    col.integer('weight').notNullable();
+    col.integer('height').notNullable();
+    col.string('sex').notNullable();
+  })
+  .createTableIfNotExists('foods', function (col) {
+    col.increments('id').primary();
+    col.string('food').notNullable();
+    col.integer('calories').notNullable();
+    col.datetime('created_at').notNullable();
+    col.integer('user_id').unsigned();
+    col.foreign('user_id').references('users.id');
+  })
+  .createTableIfNotExists('drinks', function (col) {
+    col.increments('id').primary();
+    col.string('drink').notNullable();
+    col.integer('calories').notNullable();
+    col.datetime('created_at').notNullable();
+    col.integer('user_id').unsigned();
+    col.foreign('user_id').references('users.id');
+  })
+  .createTableIfNotExists('exercises', function (col) {
+    col.increments('id').primary();
+    col.string('exercise').notNullable();
+    col.float('miles', 4, 1).notNullable();
+    col.datetime('created_at').notNullable();
+    col.integer('user_id').unsigned();
+    col.foreign('user_id').references('users.id');
+  })
+  .then(function() {
+    console.log('tables been created')
+  })
+  .catch(function(error) {
+    console.log(error);
+  })
+
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users')
+  .dropTable('foods')
+  .dropTable('drinks')
+  .dropTable('exercises');
+};
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
+const config = require('../db/knexfile.js')['development'];
+const knex = require('knex') (config);
+
 var express = require('express');
-var db = require('../db/index.js');
 
 var app = express();
 
@@ -7,13 +9,25 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('../client'));
 
-app.get('/theTruth', (req, res) => {
-	db.query('SELECT * FROM users', function(err, results) {
-		if (err) throw err;
-		res.send(results);
-		res.end();
-	});
+app.get('/secrets', (request, response) => {
+  knex.select().from('users')
+          .then(function(secrets) {
+          	console.log('fetching tg');
+            response.status(200).json(secrets);
+          })
+          .catch(function(error) {
+            console.error('somethings wrong with db')
+          });
 });
+
+
+// app.get('/theTruth', (req, res) => {
+// 	db.query('SELECT * FROM users', function(err, results) {
+// 		if (err) throw err;
+// 		res.send(results);
+// 		res.end();
+// 	});
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Balance running on port ... ', app.get('port'));
