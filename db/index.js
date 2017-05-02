ca7582b2b5156536172b1adb74946721c680227b
Fliko <baxter.flw@gmail.com>
2017-05-02 03:09:20 -0500

    I believe I can

--- db/index.js
+++ db/index.js
@@ -1,14 +1,14 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'balance'
+  host: 'us-cdbr-iron-east-03.cleardb.net',
+  user: 'ba515a4149e83a',
+  password: 'f9a72a4f',
+  database: 'heroku_ec7185f66b0bf5c'
 });
 
 connection.connect(function(err) {
-  if (err) throw err
+  if (err) console.log('why you no connect?');
   console.log('You are now connected...')
 });
 
--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS balance;
 CREATE DATABASE balance;
 
 USE balance;
