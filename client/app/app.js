9a9d78c1fd6fbdcde0d639e99d16188ee60e922b
Fliko <baxter.flw@gmail.com>
2017-05-04 10:31:56 -0500

    add angular js and start on profile

--- client/app/app.js
+++ client/app/app.js
@@ -2,11 +2,11 @@ angular.module('balance',['ngRoute'])
 	.config(function ('$routeProvider'){
 		$routeProvider
 			.when('/secrets', {//eventually will be something like '/users/:username'
-				template: profile.html,
-				controller: profileController
+				template: '/app/profile/profile.html',
+				controller: 'profileController'
 			})
 			.otherwise('/', {
-				template: home.html,
-				controller: homeCOntroller
+				template: '/app/home/home.html',
+				controller: 'homeCOntroller'
 			});
 	});
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const config = require('../db/knexfile.js')['development'];
 const knex = require('knex') (config);
-
+const path = require('path');
 var express = require('express');
 
 var app = express();
@@ -8,6 +8,10 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('../client'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '../client/index.html'));
+});
 
 app.get('/secrets', (request, response) => {
   knex.select().from('users')
@@ -20,14 +24,6 @@ app.get('/secrets', (request, response) => {
           });
 });
 
-
-// app.get('/theTruth', (req, res) => {
-// 	db.query('SELECT * FROM users', function(err, results) {
-// 		if (err) throw err;
-// 		res.send(results);
-// 		res.end();
-// 	});
-// });
 
 app.listen(app.get('port'), function() {
   console.log('Balance running on port ... ', app.get('port'));
