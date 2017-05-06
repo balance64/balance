2cfed204ef17eeed211d9f885fb2d378e5012145
Fliko <baxter.flw@gmail.com>
2017-05-06 01:31:19 -0500

    basic front end complete

--- client/app/profile/maker.js
+++ client/app/profile/maker.js
@@ -1,12 +1,8 @@
 angular.module('maker', [])
 
 .controller('makerController', ['$scope', '$location', function($scope, $location) {
-<<<<<<< 2705c1a7ee95b7ff99924253672eb19a001ccadb
   $scope.username = $location.path().split('/')[2];
   $scope.goLogin = function() {
     $location.path(`/users/${$scope.username}`)
   }
-=======
-  
->>>>>>> add profile setup
 }]);
