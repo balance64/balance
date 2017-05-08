f9cddd0bc5f475f0280c58c8328070d796087c3a
Fliko <baxter.flw@gmail.com>
2017-05-08 16:51:51 -0500

    comment and clean up

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,12 +1,8 @@
-console.log('dddddddd');
 angular.module('AuthModule', []).factory('Auth',
   function($http, $location, formEncode, currentUser, redirectLogin) {
-    //var accessToken = localStorage"";
     var accessToken = '';
     this.accessToken = '';
-    var signin = function(data) { ///data.username, data.password
-      //console.log('=====data: ', data)
-      
+    var signin = function(data) { 
 
       var config = {
         headers: {
@@ -15,33 +11,22 @@ angular.module('AuthModule', []).factory('Auth',
       }
       var username = data.username;
 
-      //nextUrl = nextUrl || '/users/' + username;
-
       data = formEncode(data);
       return $http.post('/signin', data, config)
         .then(function(response) {
-            console.log('response: ====> ', response);
             if(response.data.success == false) {
               throw new Error();
             }
 						currentUser.setUser(username, response.data.token);
-            console.log('yay!!', currentUser);
             return username;
         })
         .then(function(username){
-          console.log('sometimes this gets here')
         	$location.path('/users/' + username);
         });
     }
 
     return {
       accessToken: accessToken,
-      signin: signin,
-      // signout: function() {
-      //     this.accessToken = '';
-      // },
-      // isAuth: function() {
-      //     return this.accessToken !== '';
-      // }
+      signin: signin
     }
   });
