8379ecd69c79c6313f2d7aba0c71bf7eb2dc9916
gjblanco <gjblanco92@gmail.com>
2017-05-08 16:57:48 -0500

    comment utils.js

--- client/app/auth/utils.js
+++ client/app/auth/utils.js
@@ -1,8 +1,9 @@
 angular.module('utilsModule', [])
 
 .factory('formEncode', function() {
+	//encode data object in the right format
 	return function(data) {
-		var pairs = [];
+		var pairs = []; //stored momentarily in an array for efficiency
 		for(var name in data) {
 			pairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
 		}
@@ -13,10 +14,13 @@ angular.module('utilsModule', [])
 .factory('currentUser', function($window) {
 
 	var user = {
+		///on init, get username and password from localStorage if possible 
+		///to allow reloading of the page
 		username: $window.localStorage.getItem('balance.username') || "",
 		token: $window.localStorage.getItem('balance.token') || "",
 		loggedIn: function() {
-			//console.log(this.token);
+			//this should be consistent: if there is a user logged in, token and 
+			//username should be set, otherwise they should be empty strings
 			return this.token !== '' && this.username !== '';
 		},
 		signOut: function() {
@@ -45,7 +49,7 @@ angular.module('utilsModule', [])
 })
 
 .factory('addToken', function(currentUser, $q) { ///interceptor
-
+	//interceptors for outgoing http requests
 	var request = function(config) {
 		if(currentUser.user.loggedIn()) {
 			config.headers.Authorization = "Bearer " + currentUser.user.token;
@@ -59,7 +63,9 @@ angular.module('utilsModule', [])
 })
 
 .factory('redirectLogin', function($q, $location, currentUser) {
-
+	///if a user has to login, this function ensures that after being redirected to
+	///login and successfully logged in, the user is redirected to the page he/she
+	///originally intended to visit (said address is stored in path)
 	var path;
 
 	var responseError = function(response) {
@@ -83,6 +89,7 @@ angular.module('utilsModule', [])
 })
 
 .config(function($httpProvider) {
+	//add interceptors
 	$httpProvider.interceptors.push('addToken');
 	$httpProvider.interceptors.push('redirectLogin')
 });
