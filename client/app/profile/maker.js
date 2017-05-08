1eddfa8c8a3236a5798e61b71570619d9e70276f
gjblanco <gjblanco92@gmail.com>
2017-05-08 16:42:23 -0500

    comment profile.js

--- client/app/profile/maker.js
+++ client/app/profile/maker.js
@@ -1,17 +1,17 @@
 angular.module('maker', [])
-
+//http request that creates a user in db with the basic info
 .controller('makerController', ['$scope', '$location', '$http', function($scope, $location, $http) {
   $scope.username = $location.path().split('/')[2];
-  console.log('$http: ', $http)
   $scope.postProfile = function() {
 
       $http.post('/profile', {sex: $scope.sex, height: $scope.height, age: $scope.age, weight: $scope.weight})
       	.then(function() {
-      		console.log('posted!!!'); 
+      		console.log('posted!!!'); //instead of this something could be done in the dom to 
+                                    //alert the user about the completeness of the operation
       		$location.path(`/users/${$scope.username}`);
       	})
       	.catch(function(){
-      		console.log('not posted!! life sucks!!')
+      		console.log('not posted!! life sucks!!') //idem
       	});
   }
 
