b7b68e079fb8420d05d688dbc1237c75f64370a3
Fliko <baxter.flw@gmail.com>
2017-05-08 13:33:06 -0500

    try something

--- db/knexfile.js
+++ db/knexfile.js
@@ -2,12 +2,14 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      database: 'balance',
-      user:     'root',
-      password: ''
+      host: 'us-cdbr-iron-east-03.cleardb.net',
+      database: 'heroku_55758f1ee80e743',
+      user:     'b8a31a653b0b39',
+      password: 'ef1f2289'
     },
     seeds: {
       directory: './migrations/seeds'
-    }
+    },
+    ssl:true
   }
 };
