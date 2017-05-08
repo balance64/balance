57af125f66d643ca0904a5303ca4b3144bdf471f
Fliko <baxter.flw@gmail.com>
2017-05-08 17:04:34 -0500

    oops

--- db/knexfile.js
+++ db/knexfile.js
@@ -2,14 +2,13 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host: 'us-cdbr-iron-east-03.cleardb.net',
-      database: 'heroku_55758f1ee80e743',
-      user:     'b8a31a653b0b39',
-      password: 'ef1f2289'
+      host: 'localhost',
+      database: 'balance',
+      user:     'root',
+      password: ''
     },
     seeds: {
       directory: './migrations/seeds'
-    },
-    ssl:true
+    }
   }
 };
